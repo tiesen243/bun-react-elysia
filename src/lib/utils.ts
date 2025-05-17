51380d23509f099dba5feea2377f3e3b3c86a1d5
tiesen243 <ttien56906@gmail.com>
2025-05-18 01:57:23 +0700

    chore: app url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,5 +8,6 @@ export function cn(...inputs: ClassValue[]) {
 
 export const getBaseUrl = () => {
   if (typeof window !== 'undefined') return window.location.origin
+  if (process.env.APP_URL) return `https://${process.env.APP_URL}`
   return `http://localhost:${process.env.PORT ?? 3000}`
 }
